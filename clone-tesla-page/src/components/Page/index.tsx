4d99dfb660035a3616999ac203ffa887a35309ec
Correia <rnci@gft.com>
2022-05-10 22:38:38 -0300

    feat: init project

---
+++ clone-tesla-page/src/components/Page/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import DefaultOverlayContent from "../DefaultOverlayContent";
+
+import { Container } from "./styles";
+
+const Page: React.FC = () => {
+  return (
+    <Container>
+      <ModelsWrapper>
+        <div>
+          <ModelSection
+            modelName="Model One"
+            overlayNode={<DefaultOverlayContent label="Model One" />}
+          />
+        </div>
+      </ModelsWrapper>
+    </Container>
+  );
+};
+
+export default Page;
